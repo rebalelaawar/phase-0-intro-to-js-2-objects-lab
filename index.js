29739268884a3bae357f6ae54693402c76741b54
rebalelaawar <rebalelaawar@gmail.com>
2023-09-11 17:39:43 -0400

    objects

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+ name: "Robert",
+streetAddress: "2 Horizon Road"   
+};
+
+function updateEmployeeWithKeyAndValue(employee, key,value){
+  const cloneEmployee = {...employee}
+  cloneEmployee[key] = value
+
+  return cloneEmployee
+    
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+   employee[key] = value
+   return employee 
+
+}
+function deleteFromEmployeeByKey(employee,key){
+
+    
+   const cloneEmployee = {...employee}
+   delete cloneEmployee[key] 
+   return cloneEmployee
+   
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+   delete employee[key]
+   return employee
+
+
+}
